36363e1506b77fdf7243a575261ae65a5f0d33df
REMI DUBUS <remdubus@gmail.com>
2024-11-23 12:55:23 +0100

    feat: first commit. init react rooter, tailwind, react hook form

---
+++ src/main.tsx
@@ -0,0 +1,17 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { RouterProvider } from "react-router-dom";
+import "./index.css";
+
+const rootElement = document.getElementById("root");
+if (rootElement == null) {
+	throw new Error(`Your HTML Document should contain a <div id="root"></div>`);
+}
+
+import { mainRouter } from "./router";
+
+createRoot(rootElement).render(
+	<StrictMode>
+		<RouterProvider router={mainRouter} />
+	</StrictMode>,
+);
