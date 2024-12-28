82e49c570ed93ffb7ee5983380e0d08e6643eb75
REMI DUBUS <remdubus@gmail.com>
2024-12-28 10:11:06 +0100

    fix: bug on scroll on mobile

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,12 +1,14 @@
 import HeroBanner from "../components/HeroBanner";
+import About from "../components/About";
 import Projects from "../components/Projects";
 import Contact from "../components/Contact";
 
 export default function HomePage() {
 	return (
 		<>
-			<main className="bg-main  bg-darkcolor bg-cover bg-fixed pb-4 sm:pb-20">
+			<main className="bg-main  bg-darkcolor pb-4 sm:pb-20">
 				<HeroBanner />
+				<About />
 				<Projects />
 				<Contact />
 			</main>
