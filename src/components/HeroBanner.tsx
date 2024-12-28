16757e2764852156172784f6be0eeb5906b88613
REMI DUBUS <remdubus@gmail.com>
2024-12-28 10:01:15 +0100

    fix: bug on scoll on mobile

--- src/components/HeroBanner.tsx
+++ src/components/HeroBanner.tsx
@@ -2,11 +2,5 @@ export default function HeroBanner() {
 	// import des langues et de la fonction permetant de switcher
 
 	//JSX
-	return (
-		<header className="h-screen p-4 flex flex-col items-center justify-start overflow-hidden font-maintitlefont text-3xl text-center text-lightcolor sm:pt-80 sm:text-6xl">
-			<section className="-mt-64 h-screen w-screen bg-animation bg-no-repeat bg-center animate-spin-slow opacity-50 z-0 bg-contain sm:w-screen sm:h-full sm:-mt-96 lg:absolute lg:w-4/5 lg:top-12">
-				{" "}
-			</section>
-		</header>
-	);
+	return <h1>Rémi Dubus</h1>;
 }
