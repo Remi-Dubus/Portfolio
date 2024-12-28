d0a8832a5d302216d827bab2652c8b333606e31b
REMI DUBUS <remdubus@gmail.com>
2024-12-28 11:13:06 +0100

    fix: bug background

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -6,7 +6,7 @@ import Contact from "../components/Contact";
 export default function HomePage() {
 	return (
 		<>
-			<main className="bg-main bg-darkcolor bg-center bg-scroll bg-cover pb-4 sm:pb-20 lg:bg-fixed">
+			<main className="bg-main bg-darkcolor bg-bottom bg-scroll bg-cover pb-4 sm:pb-20 lg:bg-fixed">
 				<HeroBanner />
 				<About />
 				<Projects />
