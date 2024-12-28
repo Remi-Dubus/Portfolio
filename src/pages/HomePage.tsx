4323780e602c2a3fa2abf43f9425636c872f5353
REMI DUBUS <remdubus@gmail.com>
2024-12-28 10:05:40 +0100

    fix: bug on scoll on mobile

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,6 +1,5 @@
 import HeroBanner from "../components/HeroBanner";
 import About from "../components/About";
-import Projects from "../components/Projects";
 import Contact from "../components/Contact";
 
 export default function HomePage() {
@@ -9,7 +8,6 @@ export default function HomePage() {
 			<main className="bg-main  bg-darkcolor bg-cover bg-fixed pb-4 sm:pb-20">
 				<HeroBanner />
 				<About />
-				<Projects />
 				<Contact />
 			</main>
 		</>
