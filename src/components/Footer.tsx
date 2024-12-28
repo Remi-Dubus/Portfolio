1f3fa5802c52270a16119a5d43f6227fdf3ceaad
REMI DUBUS <remdubus@gmail.com>
2024-12-28 10:03:01 +0100

    fix: bug on scoll on mobile

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,27 +1,10 @@
-import { NavLink } from "react-router-dom";
-import { useTranslation } from "../contexts/LangContext";
-
 export default function Footer() {
 	// import des langues et de la fonction permetant de switcher
-	const { translations } = useTranslation();
 
 	//JSX
 	return (
 		<>
-			<footer className="bg-darkcolor bg-cover flex flex-col justify-center items-center h-32 border-t-2 border-lightcolor sm:text-3xl sm:h-64">
-				<span className="text-lightcolor font-textfont">
-					&copy; 2024 Rémi Dubus.
-				</span>
-				<span className="text-center text-lightcolor font-textfont">
-					{translations.footer.copyrigthText}{" "}
-					<NavLink
-						className="text-acccolor font-textfont"
-						to="http://www.freepik.com"
-					>
-						{translations.footer.copyrigthLink}
-					</NavLink>
-				</span>
-			</footer>
+			<h1>footer</h1>
 		</>
 	);
 }
--- src/components/HeroBanner.tsx
+++ src/components/HeroBanner.tsx
@@ -1,6 +1,19 @@
+import { useTranslation } from "../contexts/LangContext";
+
 export default function HeroBanner() {
 	// import des langues et de la fonction permetant de switcher
+	const { translations } = useTranslation();
 
 	//JSX
-	return <h1>Rémi Dubus</h1>;
+	return (
+		<header className="h-screen p-4 flex flex-col items-center justify-start overflow-hidden font-maintitlefont text-3xl text-center text-lightcolor sm:pt-80 sm:text-6xl">
+			<section className="-mt-64 h-screen w-screen bg-animation bg-no-repeat bg-center animate-spin-slow opacity-50 z-0 bg-contain sm:w-screen sm:h-full sm:-mt-96 lg:absolute lg:w-4/5 lg:top-12">
+				{" "}
+			</section>
+			<section className="absolute top-1/3 lg:top-1/2">
+				<h1 className="z-10 inline">{translations.hero.title}</h1>
+				<h2 className="z-10 inline text-acccolor">{translations.hero.job}</h2>
+			</section>
+		</header>
+	);
 }
