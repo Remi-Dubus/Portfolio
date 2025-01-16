b08e590749f588981edf874b74dcb8f7ab93580f
REMI DUBUS <remdubus@gmail.com>
2025-01-16 12:21:03 +0100

    feat: change the heigth of the background animation

--- src/components/HeroBanner.tsx
+++ src/components/HeroBanner.tsx
@@ -7,7 +7,7 @@ export default function HeroBanner() {
 	//JSX
 	return (
 		<header className="h-screen p-4 flex flex-col items-center justify-start overflow-hidden font-maintitlefont text-3xl text-center text-lightcolor sm:pt-80 sm:text-6xl">
-			<section className="-mt-64 h-screen w-screen bg-animation bg-no-repeat bg-center animate-spin-slow opacity-50 z-0 bg-contain sm:w-screen sm:h-full sm:-mt-96 lg:absolute lg:w-4/5 lg:top-12">
+			<section className="-mt-[1025px] h-[1700px] w-[1700px] bg-animation bg-no-repeat bg-center  animate-spin-slow opacity-25 z-0 bg-contain md:-mt-[1700px] md:h-[2200px] md:w-[2200px]">
 				{" "}
 			</section>
 			<section className="absolute top-1/3 lg:top-1/2">
